253945de4b70916ea686974e342cc57109a08fee
frnndzz <gustavoffontes07@gmail.com>
2024-11-13 16:09:46 -0300

    acabou

--- app/src/main/java/br/com/fiap/gustavofernandez_rm94382/MainActivity.kt
+++ app/src/main/java/br/com/fiap/gustavofernandez_rm94382/MainActivity.kt
@@ -4,8 +4,6 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
-import android.widget.ImageButton
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import androidx.lifecycle.ViewModelProvider
@@ -55,7 +53,7 @@ class MainActivity : AppCompatActivity() {
         }
 
         buttonIntegrantes.setOnClickListener {
-            val intent = Intent(this, AvaliacaoActivity::class.java)
+            val intent = Intent(this, IntegrantesActivity::class.java)
             startActivity(intent)
         }
 
